6e9bd0ac9da0427a697ef77418721ec194a5768d
Daedalus <manny.is.daedalus@pm.me>
2023-04-13 21:22:27 +0000

    added basic operations and functions

---
+++ simple_queue/queue.h
@@ -0,0 +1,22 @@
+#ifndef QUEUE_H
+#define QUEUE_H
+#include <stdlib.h>
+
+typedef struct queue {
+	int num;
+	char* str;
+	struct queue* next;
+} queue_t;
+
+queue_t* init(int, char*);
+queue_t* peek(queue_t**);
+queue_t* get_elem_at_index(queue_t**, int);
+void dequeue(queue_t**);
+void enqueue(queue_t**, int num, char*);
+void free_elem(queue_t*);
+size_t size_of_queue(queue_t**);
+
+/* random things added here */
+int gen_sum(queue_t**);
+
+#endif
